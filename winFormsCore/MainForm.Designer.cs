a828bfb5afb2614f6b7f73be9969a9b940fb6228
André <andrerrr@outlook.de>
2018-12-20 18:37:24 +0100

    All the good stuff

---
+++ winFormsCore/MainForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace winFormsCore
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.Location = new System.Drawing.Point(50, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(66, 27);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 289);
+            this.Controls.Add(this.button2);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ winFormsCore/MainForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winFormsCore
+{
+    public partial class MainForm : Form
+    {
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            button2.Text = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
+        }
+
+
+
+        int speedX = 2;
+        int speedY = 2;
+
+        Random ran = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            button2.Left += speedX;
+            button2.Top += speedY;
+
+            if (button2.Right >= ClientRectangle.Right || button2.Left <= 0)
+            {
+                speedX *= -1;
+                Task.Run(() => Console.Beep());
+
+                button2.BackColor = Color.FromArgb(ran.Next(255), ran.Next(255), ran.Next(255));
+                BackColor = Color.FromArgb(ran.Next(255), ran.Next(255), ran.Next(255));
+            }
+
+
+
+            if (button2.Bottom >= ClientRectangle.Bottom || button2.Top <= 0)
+            {
+                speedY *= -1;
+                Task.Run(() => Console.Beep());
+                button2.BackColor = Color.FromArgb(ran.Next(255), ran.Next(255), ran.Next(255));
+                BackColor = Color.FromArgb(ran.Next(255), ran.Next(255), ran.Next(255));
+            }
+
+        }
+    }
+}
